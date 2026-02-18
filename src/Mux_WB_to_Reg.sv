85d186e6a6cf93fc8fcf702d1ddcefb4cd6ff934
Andrew Vu <vunguyentuankiet71@gmail.com>
2026-02-18 16:28:39 -0500

    Add Mux between Writeback stage and the Register File

---
+++ src/Mux_WB_to_Reg.sv
@@ -0,0 +1,14 @@
+module Mux_WB_to_Reg #(parameter N = 32)
+(
+    input logic [N-1:0] w_ALU_Result, w_read_address, w_PC_plus_4,
+    input logic [1:0] w_ResultSrc,
+    output logic [N-1:0] w_Result
+);
+always_comb begin
+    case(w_ALU_Result)
+        2'b00: w_Result = w_ALU_Result;
+        2'b01: w_Result = w_read_address;
+        2'b10: w_Result = w_PC_plus_4;
+    endcase
+end
+endmodule
